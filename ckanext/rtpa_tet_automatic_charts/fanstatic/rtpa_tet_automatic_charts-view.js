3b29243aa5501c31bdff75e289272c2cd5b408cd
Maciej Janowski <maciej@janowski.co>
2017-12-14 16:10:14 +0000

    Ckan flag pt.2

--- ckanext/rtpa_tet_automatic_charts/fanstatic/rtpa_tet_automatic_charts-view.js
+++ ckanext/rtpa_tet_automatic_charts/fanstatic/rtpa_tet_automatic_charts-view.js
@@ -12,26 +12,12 @@ this.ckan.module('rtpa_tet_automatic_charts_view', function(jQuery, _) {
 			for(var i=0; i<data.length; i++){
 				tempcolumndata=data[i]
 				for (j=0; j<tempcolumndata.length;j++){
-					/*console.log("#"+tempcolumndata[j][0])
-					console.log(tempcolumndata[j][1])
-					console.log(tempcolumndata[j][2]);
-					var frequency=tempcolumndata[j][1];
-					var valueranges=tempcolumndata[j][2];
-					for (var a=0; a<frequency.length && a<valueranges.length; a++){
-						console.log(frequency[a])
-						console.log(valueranges[a])
-					}*/
-
 					var frequency=tempcolumndata[j][1].concat(tempcolumndata[j][0]);
 					var valueranges=tempcolumndata[j][2].concat('ranges');
-					console.log(frequency);
-					console.log(valueranges);
-					/*console.log([[tempcolumndata[j][0]]+" "+tempcolumndata[j][1]])*/
 					
 					c3.generate({
 						bindto: "#"+tempcolumndata[j][0],
 						data:{
-							/*x: [valueranges[0]],*/
 							columns:[
 							frequency
 						],
